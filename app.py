3ea8b093efa745595d5effc84d5f66b1705387b4
yukari4u <mkato4@gmail.com>
2025-05-13 23:45:32 -0300

    melissa lol

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template
+import numpy as np
+import graficos
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+@app.route('/OqueSao')
+def OqueSao():
+    graficos.matrizEnergetica()
+    graficos.crescimentoEmpregados()
+    return render_template('OqueSao.html')
+
+@app.route('/imagens/<filename>')
+def imagens(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static', 'imagens'), filename)
+
+@app.route('/hidrica')
+def hidrica():
+    graficos.energiaHidreletrica()
+    graficos.setorEnergiaHidrica()
+    return render_template('hidrica.html')
+
+@app.route('/solar')
+def solar():
+    graficos.aumentoEconomiaSolar()
+    graficos.energiaSolarSetor()
+    return render_template('solar.html')
+
+@app.route('/eolica')
+def eolica():
+    graficos.geracaoEolica()
+    return render_template('eolica.html')
+
+@app.route('/biomassa')
+def biomassa():
+    graficos.geracaoBiomassa()
+    return render_template('biomassa.html')
+
+@app.route('/geotermica')
+def geotermica():
+    graficos.geracaoGeotermica()
+    return render_template('geotermica.html')
+
+@app.route('/oceanica')
+def oceanica():
+    graficos.geracaoOceanica()
+    return render_template('oceanica.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
