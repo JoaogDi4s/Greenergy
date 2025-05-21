65745db2fd929e2c806bda739f4b26e30653e82e
RafaelaMMello <rafaelamamello@gmail.com>
2025-05-21 12:38:28 -0300

    organizando pastas e alterando imagens

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const nav = document.querySelector(".menuNavegação nav");
+    nav.classList.toggle("show");
+}
+
+// Fecha o menu ao clicar num link (em mobile)
+document.querySelectorAll(".menuNavegação nav a").forEach(link => {
+    link.addEventListener("click", () => {
+        document.querySelector(".menuNavegação nav").classList.remove("show");
+    });
+});
